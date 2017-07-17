dda906aa6b67d9ccf66c5003e34acd95206fe9af
Marcelo <marcelim015@gmail.com>
2017-07-17 16:57:21 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ TestGit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace TestGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ola mundo !");
+            Console.ReadKey();
+        }
+    }
+}
